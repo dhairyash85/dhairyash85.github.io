ca5ba7be5db1c4c97d521b62cb1f2f222fb53a73
Dhairyash Jain <jaindhairyashj@gmail.com>
2024-03-19 20:13:21 +0530

    yes

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -7,10 +7,12 @@ const Body = (props) => {
   if(projects){console.log(projects)}
   else{console.log("yes ", projects)}
   return (
-    <div className=' px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4  min-h-screen'>
-        {projects && projects.map(project=>{console.log(project) 
-          return(<Card project={project}/>)})}
-    </div>
+    <div className='px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 min-h-screen sm:min-h-auto'>
+  {projects && projects.map(project => (
+    <Card project={project} key={project.id} />
+  ))}
+</div>
+
   )
 }
 
